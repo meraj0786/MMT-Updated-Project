e984aaa13580100e1ba97720053c1d00c727d089
Meraj Ali <77686474+meraj0786@users.noreply.github.com>
2023-08-14 08:25:48 +0530

    final commit

--- src/componants/Filter.js
+++ src/componants/Filter.js
@@ -1,19 +1,84 @@
 import React, { useState } from 'react'
 import '../style/Filter.css'
 import 'semantic-ui-css/semantic.min.css'
-import { Checkbox, Item } from 'semantic-ui-react';
 
 
-function Filter({filterAirLineNames}) {
-    const [sliderValue, setsliderValue] = useState(11100);
-    const [stop1, setstop1] = useState(false);
-    const [nonstop, setNonstop] = useState(false)
+
+function Filter({ filterAirLineNames, filterFlightTypes, FilterByPrice, filterDepartureTime, filterArrivalTime }) {
+
+    const [timeSlotsA, setTimeSlotsA] = useState([
+        { name: '6', isChecked: false },
+        { name: '12', isChecked: false },
+        { name: '18', isChecked: false },
+        { name: '24', isChecked: false },
+    ]);
+
+    const handleArrival = (e) => {
+        const updatedTimeSlots = timeSlotsA.map(slot => {
+            return slot.name === e.target.value ? { name: slot.name, isChecked: !slot.isChecked } : slot;
+        })
+        setTimeSlotsA(updatedTimeSlots);
+        filterArrivalTime(updatedTimeSlots);
+
+    }
+
+
+
+    // -------------------------------------------------------------------------------------------    
+
+    // Filter by Departure Time function Logic
+    const [timeSlots, setTimeSlots] = useState([
+        { id: '6', isChecked: false },
+        { id: '12', isChecked: false },
+        { id: '18', isChecked: false },
+        { id: '24', isChecked: false },
+    ]);
+
+
+    const handleDeparture = (e) => {
+        const updatedTimeSlots = timeSlots.map(slot => {
+            return slot.id === e.target.value ? { id: slot.id, isChecked: !slot.isChecked } : slot;
+        })
+        setTimeSlots(updatedTimeSlots);
+
+        filterDepartureTime(updatedTimeSlots);
+
+    }
+
+
+
+    // ---------------------------------------------------------------------------------------
+    //Filter by Price slider functions logic
+    const [sliderValue, setsliderValue] = useState(5000);
     let price = `₹${sliderValue}`;
 
+    const handlePrice = (e) => {
+        setsliderValue(e.target.value)
+        FilterByPrice(sliderValue)
+    }
 
-    // for airline name Checkbox
-    const [airLineChecked, setAirLineChecked] = useState([]);
 
+    // ---------------------------------------------------------------------------
+
+    // Filter by Stop and non stop checkbox function logic
+    const [checkboxes, setCheckboxes] = useState({
+        checkbox1: false,
+        checkbox2: false
+    });
+
+    const handleCheckboxChange = (event) => {
+        const checkboxId = event.target.id;
+        setCheckboxes((prevCheckboxes) => ({
+            ...prevCheckboxes,
+            [checkboxId]: !prevCheckboxes[checkboxId],
+        }));
+
+        checkboxId === 'checkbox1' ? filterFlightTypes(checkboxes.checkbox1) : filterFlightTypes(checkboxes.checkbox2)
+
+    };
+
+    // ---------------------------------------------------------------------------
+    // Filter by Airline Names checkbox Function Logics
     const [airLineNames, setAirLineNames] = useState([
         {
             name: 'Air India',
@@ -37,13 +102,14 @@ function Filter({filterAirLineNames}) {
         }])
 
 
-    const handAirlineNameChange = (e)=>{
+    const handAirlineNameChange = (e) => {
         const newAirLineNames = airLineNames.map(airLineName => {
-            return airLineName.name === e.target.value ? {name: airLineName.name, isSelected: !airLineName.isSelected} : airLineName;
+            return airLineName.name === e.target.value ? { name: airLineName.name, isSelected: !airLineName.isSelected } : airLineName;
         })
         setAirLineNames(newAirLineNames);
         filterAirLineNames(newAirLineNames);
     }
+    // ----------------------------------------------------------------------------------------
 
     return (
         <>
@@ -54,63 +120,88 @@ function Filter({filterAirLineNames}) {
                 {/* one way price slider */}
                 <div className="slider-container">
                     <label htmlFor="customRange1" className="form-label" style={{ fontWeight: 'bold' }}>One Way Price</label>
-                    <input type="range" onChange={(e) => { setsliderValue(e.target.value) }} min="4800" max="17400" step='300' value={sliderValue} className='form-range' id="customRange1" data-tooltip={price} data-position="top center" />
+                    <input type="range" onChange={(e) => { handlePrice(e) }} min="3000" max="7000" step='50' value={sliderValue} className='form-range' id="customRange1" data-tooltip={price} data-position="top center" />
                     <div className="p-container" >
-                        <p>₹4800</p>
-                        <p>₹17400</p>
+                        <p>₹3000</p>
+                        <p>₹7000</p>
                     </div>
                 </div>
+                {/* -------------------------------------------------------------------------------------------------- */}
 
                 {/* Stop and Non-stop */}
                 <div className="stop-container">
                     <h4>Stop and Non Stop</h4>
-                    <Checkbox onChange={() => { setstop1(!stop1) }} label='1 Stop' style={{ marginTop: '6px' }} />
-                    <Checkbox onChange={() => { setNonstop(!nonstop) }} label='Non Stop' style={{ marginTop: '15px' }} />
+                    <div>
+                        <div>
+                            <label>
+                                <input
+                                    type="checkbox"
+                                    id="checkbox1"
+                                    checked={checkboxes.checkbox1}
+                                    onChange={handleCheckboxChange}
+                                />
+                                Stop1
+                            </label>
+                        </div>
+                        <div>
+                            <label>
+                                <input
+                                    type="checkbox"
+                                    id="checkbox2"
+                                    checked={checkboxes.checkbox2}
+                                    onChange={handleCheckboxChange}
+                                />
+                                Non Stop
+                            </label>
+                        </div>
+                    </div>
                 </div>
+
+                {/* --------------------------------------------------------------------------------------------------------------*/}
 
                 {/* Departure time */}
                 <div className="departure-arrival-container">
                     <h4>Departure Timing</h4>
                     <div className="btn-group time-range-container" role="group" aria-label="Basic checkbox toggle button group">
-                        <input type="checkbox" className="btn-check" id="btncheck1" autoComplete="off" />
-                        <label className="btn btn-outline-primary font-size" htmlFor="btncheck1">Before 6 AM</label>
+                        <input type="checkbox" onChange={(e) => { handleDeparture(e) }} checked={timeSlots.isChecked} value='6' className="btn-check" id="6" />
+                        <label className="btn btn-outline-primary font-size" htmlFor="6"> Before 6 AM</label>
 
-                        <input type="checkbox" className="btn-check" id="btncheck2" autoComplete="off" />
-                        <label className="btn btn-outline-primary font-size" htmlFor="btncheck2">6 AM-12 PM</label>
+                        <input type="checkbox" onChange={(e) => { handleDeparture(e) }} checked={timeSlots.isChecked} className="btn-check" value='12' id="12" />
+                        <label className="btn btn-outline-primary font-size" htmlFor="12">6 AM-12 PM</label>
 
-                        <input type="checkbox" className="btn-check" id="btncheck3" autoComplete="off" />
-                        <label className="btn btn-outline-primary font-size" htmlFor="btncheck3">12 PM-6 PM</label>
+                        <input type="checkbox" onChange={(e) => { handleDeparture(e) }} checked={timeSlots.isChecked} className="btn-check" value='18' id="18" />
+                        <label className="btn btn-outline-primary font-size" htmlFor="18">12 PM-6 PM</label>
 
-                        <input type="checkbox" className="btn-check" id="btncheck4" autoComplete="off" />
-                        <label className="btn btn-outline-primary font-size" htmlFor="btncheck4">After 6 PM</label>
+                        <input type="checkbox" onChange={(e) => { handleDeparture(e) }} checked={timeSlots.isChecked} className="btn-check" value='24' id="24" />
+                        <label className="btn btn-outline-primary font-size" htmlFor="24">After 6 PM</label>
                     </div>
                 </div>
-
+                {/* ---------------------------------------------------------------------------------------------------------------- */}
                 {/* Arrival time */}
                 <div className="departure-arrival-container">
                     <h4>Arrival Timing</h4>
                     <div className="btn-group time-range-container" role="group" aria-label="Basic checkbox toggle button group">
-                        <input type="checkbox" className="btn-check" id="btncheck5" autoComplete="off" />
-                        <label className="btn btn-outline-primary font-size" htmlFor="btncheck5">Before 6 AM</label>
+                        <input type="checkbox" onChange={(e) => { handleArrival(e) }} className="btn-check" id="6A" checked={timeSlotsA.isChecked} value='6' />
+                        <label className="btn btn-outline-primary font-size" htmlFor="6A">Before 6 AM</label>
 
-                        <input type="checkbox" className="btn-check" id="btncheck6" autoComplete="off" />
-                        <label className="btn btn-outline-primary font-size" htmlFor="btncheck6">6 AM-12 PM</label>
+                        <input type="checkbox" onChange={(e) => { handleArrival(e) }} className="btn-check" id="12A" checked={timeSlotsA.isChecked} value='12' />
+                        <label className="btn btn-outline-primary font-size" htmlFor="12A">6 AM-12 PM</label>
 
-                        <input type="checkbox" className="btn-check" id="btncheck7" autoComplete="off" />
-                        <label className="btn btn-outline-primary font-size" htmlFor="btncheck7">12 PM-6 PM</label>
+                        <input type="checkbox" onChange={(e) => { handleArrival(e) }} className="btn-check" id="18A" checked={timeSlotsA.isChecked} value='18' />
+                        <label className="btn btn-outline-primary font-size" htmlFor="18A">12 PM-6 PM</label>
 
-                        <input type="checkbox" className="btn-check" id="btncheck8" autoComplete="off" />
-                        <label className="btn btn-outline-primary font-size" htmlFor="btncheck8">After 6 PM</label>
+                        <input type="checkbox" onChange={(e) => { handleArrival(e) }} className="btn-check" id="24A" checked={timeSlotsA.isChecked} value='24' />
+                        <label className="btn btn-outline-primary font-size" htmlFor="24A">After 6 PM</label>
                     </div>
                 </div>
-
+                {/* ---------------------------------------------------------------------------------------------------------------- */}
                 {/* {Airline by name code } */}
                 <div className="airline-container">
                     <h3>Airlines</h3>
 
                     {airLineNames.map((airLine, index) => {
                         return (
-                            <div>
+                            <div key={index}>
                                 <input id={`${airLine.name}-${index}`} key={index} type="checkbox" checked={airLine.isSelected} value={airLine.name} name={airLine.name} onChange={e => handAirlineNameChange(e)} />
                                 <label htmlFor={`${airLine.name}-${index}`}>{airLine.name}</label>
                             </div>
